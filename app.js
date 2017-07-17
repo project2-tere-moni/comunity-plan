222f3f91cbb564917c16d8ce654351e289c14cf5
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-17 13:08:57 +0200

    preparing baseline

--- app.js
+++ app.js
@@ -7,14 +7,22 @@ const bodyParser   = require('body-parser');
 const layouts      = require('express-ejs-layouts');
 const mongoose     = require('mongoose');
 
+const index = require('./routes/index');
+const user = require('./routes/user');
+const auth = require('./routes/auth');
+const events = require('./routes/event');
+const place = require('./routes/place');
+const {dbURL} = require('./config/db');
 
-mongoose.connect('mongodb://localhost/comunity-plan');
+mongoose.connect(dbURL);
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(layouts);
+app.set('layouts', 'layouts/main');
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
@@ -26,10 +34,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(layouts);
 
-const index = require('./routes/index');
+
 app.use('/', index);
+app.use('/user', user);
+app.use('/event', events);
+app.use('/place', place);
+app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
