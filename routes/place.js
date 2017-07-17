b7a100278cca1ddca3b2a050befc2734b43a04e3
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-17 20:53:53 +0200

    user routes and views, place route and view

--- routes/place.js
+++ routes/place.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Place = require('../models/Place');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: "index"
-  });
+  Place.find({}, (err, places) => {
+      console.log(places);
+      if (err) {
+          next();
+          return err;
+        } else {
+          res.render('place/index', {places: places});
+        }
+    });
 });
 
 module.exports = router;
