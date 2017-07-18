479a1f547319a64f4fc388930a2fad33334f8e3c
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-07-18 14:49:57 +0200

    edit event complete

--- models/Event.js
+++ models/Event.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EventSchema = Schema({
-  name: String,
-  description: String,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
   place_id: String,
-  date: Date,
-  creator_id: String,
-  picPath: String
+  deadline: { type: Date, required: true },
+  creator_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  picPath: String,
+  goal          : { type: Number, required: true },
+  totalPledged  : { type: Number, default: 0 }
 });
 
 const Event = mongoose.model('Event', EventSchema);
