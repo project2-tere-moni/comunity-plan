f3c221c12cc2258ac8d66c8ce0886a51150f22bb
teresatorrijos <torrijosgarcia.teresa@gmail.com>
2017-07-19 19:57:12 +0200

    new event fixed

--- bin/seed.js
+++ bin/seed.js
@@ -7,61 +7,61 @@ mongoose.connect('mongodb://localhost/comunity-plan')
     console.log('seed place');
     let places = [
       {
-        place_id: 1,
+        place_id: 'Pista',
         description: 'type:edificación, m2: 200, address: Calle Pista, 4, 28011 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.41573, -3.7272500000000264] },
         picPath:'/images/1.png'
       },
       {
-        place_id: 2,
+        place_id: 'Paseo de la Chopera',
         description: 'type: solar, m2: 200, address: Paseo de la chopera, 28045 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.39411282476374, -3.6999088525772095] },
         picPath:'/images/2.png'
       },
       {
-        place_id: 3,
+        place_id: 'Rosa Jardón',
         description: 'type: solar, m2: 200, address: Calle Rosa Jardón, 12, 28016 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.46517848053278, -3.668789863586426] },
         picPath:'/images/3.png'
       },
       {
-        place_id: 4,
+        place_id: 'Rafael Finat',
         description: 'type: edificacion, m2: 200, address: Calle Rafael Finat, 75, 28044 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.3779847, -3.7710730000000012] },
         picPath:'/images/4.png'
       },
       {
-        place_id: 5,
+        place_id: 'Mayor',
         description: 'type: edificacion, m2: 200, address: Calle Mayor, 58, 28013 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.4158009, -3.709655399999974] },
         picPath:'/images/5.png'
       },
       {
-        place_id: 6,
+        place_id: 'Señores de Luzon',
         description: 'type: edificacion, m2: 200, address: Calle de los Señores de Luzon, 3, 28013 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.4158377, -3.710760299999947] },
         picPath:'/images/6.png'
       },
       {
-        place_id: 7,
+        place_id: 'Ribera de Curtidores',
         description: 'type: edificacion, m2: 200, address: Calle de la Ribera de Curtidores, 8, 28005 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.4087647, -3.7074706999999307] },
         picPath:'/images/7.png'
       },
       {
-        place_id: 8,
+        place_id: 'Guatemala',
         description: 'type: edificacion, m2: 200, address: Calle de Guatemala, 24, 28016 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.4592734, -3.6705498000000034] },
         picPath:'/images/8.png'
       },
       {
-        place_id: 9,
+        place_id: 'Eduardo Morales',
         description: 'type: solar, m2: 200, address: Calle Eduardo Morales, 28, 28025 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.3878416, -3.7373943000000054] },
         picPath:'/images/9.png'
       },
       {
-        place_id: 10,
+        place_id: 'Hermanos Álvarez Quintero',
         description: 'type: solar, m2: 200, address: Calle Hermanos Álvarez Quintero, 3, 28004 Madrid, Spain',
         location: { type: 'Point', coordinates: [40.4275401, -3.6974347000000307] },
         picPath:'/images/10.png'
