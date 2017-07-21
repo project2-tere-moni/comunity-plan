c6cb9d4e4b0270d883774331d59499d64045e019
Monica Calderaro <monicacalderaro@Monicas-MacBook-Air.local>
2017-07-21 10:46:11 +0200

    final changes

--- bin/placesSeed.js
+++ bin/placesSeed.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const Place = require('../models/Place');
 const mongoose = require('mongoose');
 const dbURL = process.env.MONGO_DB_URL;
 
-mongoose.connect('mongodb://localhost/comunity-plan')
+mongoose.connect(dbURL)
   .then(() => {
     console.log('seed place');
     let places = [
